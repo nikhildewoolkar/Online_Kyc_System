ddad920dff51f3607b99bc7ae0b458f2f55f0e16
Nikhil Dewoolkar <dewoolkarnikhil29@gmail.com>
2021-09-09 13:24:38 +0530

    Update settings.py

--- kyc/settings.py
+++ kyc/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'quadrubalsquad29@gmail.com'
-EMAIL_HOST_PASSWORD = '5v4w3x2y1z'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
